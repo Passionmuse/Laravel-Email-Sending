39c2a108b516ec9a33c975532a646e719f55444c
Passionmuse <passionmuse1216@gmail.com>
2024-06-14 11:04:03 +0900

    add route for sending email

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MailController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// email send
+Route::get ('/',[MailController::class,'mailform']);
+Route::post ('/send-mail',[MailController::class,'maildata'])->name('send_mail');
